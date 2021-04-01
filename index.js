cab039f009e8b08d8b9a758a25ce197ed5f309a0
higavip <somfan3@gmail.com>
2021-04-01 11:41:42 +0700

    aaa

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ app.use(express.static('assets'));
 
 
 
-app.listen(process.env.PORT || '3002');
-
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Way to go server at port " + port);
+});
